8c279056b4c99f981ebdbf66ac9409c5eb1269af
Bora Kim <borakim@192.168.1.100>
2020-12-21 13:02:28 +0000

    Added new components to render basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import KanjiList from './component/KanjiList';
+import Nav from './component/Nav';
+import Main from './component/Main';
+import Footer from './component/Footer';
+
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Main />
       <KanjiList />
+      <Footer />
     </div>
   );
 }
---
+++ src/component/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class componentName extends Component {
+  render() {
+    return (
+        <div>
+            <h1>Let's learn JPLT level 2 words with us</h1>
+            <button type="button" className="btn btn-primary">Beginner</button>
+            <button type="button" className="btn btn-primary">Intermidiate</button>
+            <button type="button" className="btn btn-primary">Advanced</button>
+        </div>
+    )
+  }
+}
