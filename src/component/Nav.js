72f81eeec4e716e50e89142c0e04a8048b5286bc
Bora Kim <stellakkkk@hotmail.com>
2021-01-02 17:00:25 +0000

    Enhanced responsive style and new Navbar adopted

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,34 +1,53 @@
 import React, { Component } from 'react'
 import NavDropdown from "react-bootstrap/NavDropdown"
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 export default class componentName extends Component {
   render() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <a className="navbar-brand" href="/">Kanji Suru</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarColor01">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="/">Home
-                  <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              <li className="nav-item">
-              <NavDropdown title="Kanji Level" id="basic-nav-dropdown">
+        // <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        //   <a className="navbar-brand" href="/">Kanji-ru<br></br>漢字る</a>
+        //   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        //     <span className="navbar-toggler-icon"></span>
+        //   </button>
+        //   <div className="collapse navbar-collapse" id="navbarColor01">
+        //     <ul className="navbar-nav mr-auto">
+        //       <li className="nav-item active">
+        //         <a className="nav-link" href="/">Home
+        //           <span className="sr-only">(current)</span>
+        //         </a>
+        //       </li>
+        //       <li className="nav-item">
+        //       <NavDropdown title="Kanji Level" id="basic-nav-dropdown">
+        //         <NavDropdown.Item href="/beginner">Beginner</NavDropdown.Item>
+        //         <NavDropdown.Item href="/intermediate">Intermediate</NavDropdown.Item>
+        //         <NavDropdown.Item href="/advanced">Advanced</NavDropdown.Item>
+        //       </NavDropdown>
+        //       </li>
+        //       <li className="nav-item">
+        //         <a className="nav-link" href="/">Bookmark</a>
+        //       </li>
+        //     </ul>
+        //   </div>
+        // </nav>
+
+        <Navbar expand="lg" className="navbar" >
+          <Navbar.Brand href="/" >Kanji-ru<br></br>漢字る</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/" style={{color: "red"}}>Home</Nav.Link>
+              <NavDropdown title="Level" id="basic-nav-dropdown" >
                 <NavDropdown.Item href="/beginner">Beginner</NavDropdown.Item>
                 <NavDropdown.Item href="/intermediate">Intermediate</NavDropdown.Item>
                 <NavDropdown.Item href="/advanced">Advanced</NavDropdown.Item>
+                
+                <NavDropdown.Divider />
               </NavDropdown>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">Bookmark</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
     )
   }
 }
